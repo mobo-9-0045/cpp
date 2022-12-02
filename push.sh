d25c6346fcb2d32ae3e1f3b63d15a1219f344124
mobo-9-0045 <aomman@aomman>
2022-12-02 22:16:49 +0000

    02/12/2022

--- push.sh
+++ push.sh
@@ -1,11 +1,15 @@
 echo "Please enter today date in this form : /dd/mm/yyyy : "
 
 read date
-
+echo "git add ."
 git add .
+
+echo "git commit -m (date)"
 git commit -m "${date}"
 
+echo "git satus"
 git status
 
+echo "push"
 git push git@github.com:mobo-9-0045/cpp.git
 
