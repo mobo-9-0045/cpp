30792c2a359ca1c1f2021e7a046aeb7e9d000631
mobo-9-0045 <aomman@aomman>
2022-12-03 20:58:04 +0000

    03/12/2022

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aomman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/02 16:00:11 by aomman            #+#    #+#             */
-/*   Updated: 2022/12/02 16:00:12 by aomman           ###   ########.fr       */
+/*   Created: 2022/12/01 18:41:00 by aomman            #+#    #+#             */
+/*   Updated: 2022/12/02 13:25:30 by aomman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FIXED_HPP
-# define FIXED_HPP
-# include <iostream>
+#include <iostream>
+#include "Fixed.hpp"
 
-class Fixed
+int main()
 {
-	private :
-		int fixed_point;
-		static const int bits = 8;
-	public :
-		Fixed();
-		Fixed(int fixed_point);
-		Fixed(Fixed &f);
-		Fixed &operator=(Fixed &f);
-		~Fixed();
-		int 	getRawBits(void) const;
-		void	setRawBits(int const raw);
-};
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
 
-#endif
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
