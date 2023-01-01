255709159a395556f5402d14b0e04348528c93d2
mobo-9-0045 <98918707+mobo-9-0045@users.noreply.github.com>
2023-01-01 12:02:09 -0600

    Update main.c

--- cpp06/ex01/main.c
+++ cpp06/ex01/main.c
@@ -1,6 +1,24 @@
-#include<iostream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aomman <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/30 10:22:52 by aomman            #+#    #+#             */
+/*   Updated: 2023/01/01 11:12:38 by aomman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"serialize.hpp"
 
 int main(void)
 {
+	Data *data = new Data();
+
+	std::cout << data << std::endl;
+	uintptr_t ptr = serialize(data);
+	Data *dataptr = deserialize(ptr);
+	std::cout << dataptr << std::endl;
 	return (0);
 }
