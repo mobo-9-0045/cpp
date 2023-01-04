2500970a4770577aafb2fc1797b85c7a2e640434
Abdelilah Ommane <aomman@e2r10p4.1337.ma>
2023-01-04 21:20:47 +0100

    04/01/2023

--- cpp07/ex01/iter.cpp
+++ cpp07/ex01/iter.cpp
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   iter.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aomman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 21:01:32 by aomman            #+#    #+#             */
-/*   Updated: 2023/01/04 21:14:34 by aomman           ###   ########.fr       */
+/*   Created: 2023/01/04 21:17:48 by aomman            #+#    #+#             */
+/*   Updated: 2023/01/04 21:19:40 by aomman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"iter.hpp"
 
-int main(void)
+T	iter(T &arr, int size, T f())
 {
-	char *arr;
-
-	arr = malloc(10);
-	arr = "moboman";
-	iter(arr, 7, //I should search about function as parameter in a function ^_^
-	return (0);
+	//i should search to remember about function as parameter in c++
+	return (void)T = 1;
 }
-
 
--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,25 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aomman <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/04 21:01:32 by aomman            #+#    #+#             */
-/*   Updated: 2023/01/04 21:14:34 by aomman           ###   ########.fr       */
+/*   Created: 2023/01/04 21:04:05 by aomman            #+#    #+#             */
+/*   Updated: 2023/01/04 21:19:59 by aomman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"iter.hpp"
+#ifndef ITER_HPP
+# define ITER_HPP
+# include<iostream>
 
-int main(void)
-{
-	char *arr;
+template <class T>
+T	iter(char &arr, int size, T f());
 
-	arr = malloc(10);
-	arr = "moboman";
-	iter(arr, 7, //I should search about function as parameter in a function ^_^
-	return (0);
-}
-
+#endif
 
